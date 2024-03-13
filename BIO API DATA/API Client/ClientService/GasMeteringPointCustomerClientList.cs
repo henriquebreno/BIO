236a80ed2ddd3adfcc160c6bbd102280679b46da
zordanian <simon@zorde.dk>
2024-03-13 19:06:39 +0100

    update

--- BIO API DATA/API Client/ClientService/GasMeteringPointCustomerClientList.cs
+++ BIO API DATA/API Client/ClientService/GasMeteringPointCustomerClientList.cs
@@ -30,6 +30,12 @@ namespace BIO_API_DATA.API_Client
 
 		public async Task<List<GasMeterPointCustomerModel>> GetGasmeteringPointCustomerassociation(List<string> CustomerIdList)
 		{
+
+			_customersClient.GetAllCustomers();
+
+			var CustomerIdList = TopLevelCustomersClientList._allCustomerIds;
+
+
 			var GasMeterPointCustomerIDList = new List<GasMeterPointCustomerModel>();
 
 			string url;
